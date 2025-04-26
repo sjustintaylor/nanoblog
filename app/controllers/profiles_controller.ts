a5599feadc0d0b3ce22b30b924c465e37c326833
Justin Taylor <26264269+sjustintaylor@users.noreply.github.com>
2025-04-26 15:05:28 +1000

    feat: checkpoint

--- app/controllers/profiles_controller.ts
+++ app/controllers/profiles_controller.ts
@@ -22,7 +22,7 @@ export default class ProfilesController {
     // TODO: fetch user record
     return ctx.view.render('pages/profile', {
       username: ctx.params.id,
-      bio: "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?",
+      bio: "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the man talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?",
       posts: [
         {
           date: DateTime.fromSeconds(1714109408).toLocaleString(DateTime.DATE_FULL),
